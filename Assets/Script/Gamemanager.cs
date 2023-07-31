5c9e4c1f4e02a3b204ace1d96a7e17f59d86cf95
KAITO <90747002+HishitaniKaito@users.noreply.github.com>
2023-07-31 12:20:18 +0900

    test

---
+++ Assets/Script/Gamemanager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public int _p_hp = 100;
+    public int _e_hp = 500;
+    public int _p_sp = 100;
+    public int _p_limhp = 100;
+    public int _e_limhp = 100;
+    public int _p_limsp = 100;
+    public int _skill = 0;
+    int _item = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _p_hp = _p_limhp;
+        _p_sp = _p_limsp;
+        _e_hp = _e_limhp;
+        _skill = 0;
+        _item = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Kougeki()
+    {
+        _e_hp -= 50;
+        EnemyTurn();
+    }
+    public void Skill()
+    {
+        if(_p_sp > 20)
+        {
+            _skill = 3;
+            _p_sp -= 20;
+        }
+        else
+        {
+            Debug.Log("sp切れてるから使えない");
+        }
+        EnemyTurn();
+        //aaaaa
+    }
+    public void Item()
+    {
+        if(_item > 0)
+        {
+            _p_hp += 20;
+            if (_p_hp > _p_limhp)
+            {
+                _p_hp = _p_limhp;
+            }
+            _item--;
+        }
+        else
+        {
+            Debug.Log("アイテムないよ");
+        }
+        EnemyTurn();
+    }
+    public void EnemyTurn()
+    {
+        if(_skill > 0)
+        {
+            int a = Random.Range(1, 5);
+            if(a > 1)
+            {
+                _e_hp -= 50;
+                Debug.Log("反射した");
+                _skill--;
+            }
+        }
+        else
+        {
+            _p_hp -= 50;
+        }
+        Debug.Log("敵のHP="+_e_hp+",自分のHP="+_p_hp);
+    }
+}
